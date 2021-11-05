dd773a1a51cb90471026aa863e871996f459f0a3
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-11-05 15:58:37 +0300

    Update firebase.js

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxc46_vuzRh2tiyY4kRnTVp981eHP-SSE",
-  authDomain: "portfolio-88cee.firebaseapp.com",
-  projectId: "portfolio-88cee",
-  storageBucket: "portfolio-88cee.appspot.com",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "1007456522598",
-  appId: "1:1007456522598:web:938fa7a15576f7c48638a7",
+  appId: "",
   measurementId: "G-RFRYVTKEQY"
 };
 
